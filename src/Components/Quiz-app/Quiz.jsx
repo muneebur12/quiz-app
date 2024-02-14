1b64b932883dfb070b0665261ab5b2e981882d94
Muneeb Ashiq <muneebashiq88@gmail.com>
2024-02-14 21:50:41 +0500

    Style the html page

--- src/Components/Quiz-app/Quiz.jsx
+++ src/Components/Quiz-app/Quiz.jsx
@@ -5,7 +5,7 @@ const Quiz = () => {
     <div className='container'>
       <h1>Quiz App</h1>
       <hr />
-      <h2>Which device is required for internet connection?</h2>
+      <h2>1. Which device is required for internet connection?</h2>
       <ul>
         <li>Modem</li>
         <li>Router</li>
