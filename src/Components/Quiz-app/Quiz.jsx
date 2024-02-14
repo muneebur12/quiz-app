d2aac1135f823d42275b4c65256e08b82244bb4b
Muneeb Ashiq <muneebashiq88@gmail.com>
2024-02-14 21:12:06 +0500

    Design the Html Page

--- src/Components/Quiz-app/Quiz.jsx
+++ src/Components/Quiz-app/Quiz.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 import './Quiz.css'
 const Quiz = () => {
   return (
-    <div>
-      
+    <div className='container'>
+      <h1>Quiz App</h1>
+      <hr />
+      <h2>Which device is required for internet connection?</h2>
+      <ul>
+        <li>Modem</li>
+        <li>Router</li>
+        <li>Lan Cable</li>
+        <li>Pen Drive</li>
+      </ul>
+      <button>Next</button>
+      <div className="index">
+        1 of 5 questions.
+      </div>
     </div>
   )
 }
